09e10f145573740f05deb525b1de71e8ddbae3f1
Yan <janusle@gmail.com>
2015-04-13 01:16:25 +1000

    fixed the issues in the model

--- yanlog/common/models.py
+++ yanlog/common/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class Page(models.Model):
 	name = models.CharField(max_length=128)
 	link = models.URLField()
-	is_display_on_home = models.BooleanField
+	is_display_on_home = models.BooleanField(Default=False)
     
 class Setting(models.Model):
     avatar = models.ImageField()
-    
+
 
