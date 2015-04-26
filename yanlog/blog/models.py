2cb463ce6a27d9661941115626406cb5fc9eb868
janusle <janusle@gmail.com>
2015-04-26 12:04:21 +1000

    updated admin.py

--- yanlog/blog/models.py
+++ yanlog/blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
+@python_2_unicode_compatible
 class Post(models.Model):
     title = models.CharField(max_length=100, blank=True)
     lang = models.CharField(max_length=2)
@@ -8,10 +10,21 @@ class Post(models.Model):
     category = models.ForeignKey('Category')
     tags = models.ManyToManyField('Tag')
 
+    def __str__(self):
+        return self.title	
 
+
+@python_2_unicode_compatible
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+    	return self.name
 
+
+@python_2_unicode_compatible
 class Tag(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+    	return self.name
