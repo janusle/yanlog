97811a2db0d5335dafcf0e1e56b83042c20230c4
janusle <janusle@gmail.com>
2015-12-12 00:01:21 +1100

    update uwsgi conf

--- yanlog/yanlog/settings.py
+++ yanlog/yanlog/settings.py
@@ -21,9 +21,7 @@ class Base(cbs.BaseSettings):
 
     SITE_ID = 1
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    # Quick-start development settings - unsuitable for production
-    # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
-    # SECURITY WARNING: keep the secret key used in production secret!
+
     ALLOWED_HOSTS = []
     # Application definition
     INSTALLED_APPS = (
